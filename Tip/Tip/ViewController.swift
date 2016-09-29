ca7deabe139f631cafb597f9686e12812df18927
David Lashkhi <davidlashkhi@Davids-MacBook-Pro.local>
2016-09-29 19:02:28 +0300

    Changes for gesture recognisers

--- Tip/Tip/ViewController.swift
+++ Tip/Tip/ViewController.swift
@@ -13,41 +13,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var amount: UITextField!
     @IBOutlet weak var totalSum: UILabel!
     @IBOutlet weak var tipPercentage: UILabel!
-    @IBOutlet var rightSwipe: UISwipeGestureRecognizer!
-    @IBOutlet var leftSwipe: UISwipeGestureRecognizer!
+    @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
     var percentage: Int = 0;
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureGestureRecognizers()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
-    fileprivate func configureGestureRecognizers() {
-        rightSwipe.addTarget(self, action: #selector(changePercentage))
-        leftSwipe.addTarget(self, action: Selector("changePercentage:"))
-    
-    }
-    
-     func changePercentage(sender: UISwipeGestureRecognizer) {
-        switch sender.direction {
-        case UISwipeGestureRecognizerDirection.right:
-            percentage += 1
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        var startValue = 0
+        switch sender.state {
+        case .began:
+            startValue = Int(translation.x)
             break
-        case UISwipeGestureRecognizerDirection.left:
-            percentage -= 1
-            break
+        case .ended:
+            
         default:
-            break
+            <#code#>
         }
-        tipPercentage.text = String(percentage)
+        if sender.state == .began {
+            startValue = Int(translation.x)
+        }
+        
+        if percentage >= 0 {
+            percentage = startValue + Int(translation.x/100)
+            print(translation)
+            tipPercentage.text = String(percentage)
+        }
     }
+
+
 
 }
 
